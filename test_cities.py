fb3f009338cb315183a5e4ace7776be8066475e2
HPRIOR <hprior02@dcs.bbk.ac.uk>
2019-10-22 18:45:49 +0100

    added test for swap_cities

--- test_cities.py
+++ test_cities.py
@@ -13,8 +13,22 @@ def test_compute_total_distance():
     '''add your further tests'''
 
 def test_swap_cities():
+    road_map1 = [("Kentucky", "Frankfort", 38.197274, -84.86311),
+                 ("Delaware", "Dover", 39.161921, -75.526755),
+                 ("Minnesota", "Saint Paul", 44.95, -93.094)]
+    index1 = 0
+    index2 = 1
+    assert swap_cities(road_map1, index1, index2) == [("Delaware", "Dover", 39.161921, -75.526755),
+                                                        ("Kentucky", "Frankfort", 38.197274, -84.86311),
+                                                       ("Minnesota", "Saint Paul", 44.95, -93.094)]
+
     '''add your tests'''
 
 def test_shift_cities():
+    road_map1 = [("Kentucky", "Frankfort", 38.197274, -84.86311),
+                 ("Delaware", "Dover", 39.161921, -75.526755),
+                 ("Minnesota", "Saint Paul", 44.95, -93.094)]
+
+
     '''add your tests'''
 
