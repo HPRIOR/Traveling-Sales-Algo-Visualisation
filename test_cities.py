d78e997dbcb83155c241398d629359023812d92e
HPRIOR <hprior02@dcs.bbk.ac.uk>
2019-10-22 20:07:37 +0100

    shift cities now working with first test case

--- test_cities.py
+++ test_cities.py
@@ -29,9 +29,9 @@ def test_shift_cities():
                  ("Delaware", "Dover", 39.161921, -75.526755),
                  ("Minnesota", "Saint Paul", 44.95, -93.094)]
 
-    assert shift_cities(road_map1) == [("Delaware", "Dover", 39.161921, -75.526755),
-                                       ("Kentucky", "Frankfort", 38.197274, -84.86311),
-                                       ("Minnesota", "Saint Paul", 44.95, -93.094)]
+    assert shift_cities(road_map1) == [("Minnesota", "Saint Paul", 44.95, -93.094),
+                                       ("Delaware", "Dover", 39.161921, -75.526755),
+                                       ("Kentucky", "Frankfort", 38.197274, -84.86311)]
 
 
 
