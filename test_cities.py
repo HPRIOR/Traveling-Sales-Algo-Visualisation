2dfbfa00020357b8fd49fa655e674efc4c9ebbcc
HPRIOR <hprior02@dcs.bbk.ac.uk>
2019-10-22 20:01:45 +0100

    added simple test for shift cities

--- test_cities.py
+++ test_cities.py
@@ -19,8 +19,8 @@ def test_swap_cities():
     index1 = 0
     index2 = 1
     assert swap_cities(road_map1, index1, index2) == [("Delaware", "Dover", 39.161921, -75.526755),
-                                                        ("Kentucky", "Frankfort", 38.197274, -84.86311),
-                                                       ("Minnesota", "Saint Paul", 44.95, -93.094)]
+                                                      ("Kentucky", "Frankfort", 38.197274, -84.86311),
+                                                      ("Minnesota", "Saint Paul", 44.95, -93.094)]
 
     '''add your tests'''
 
@@ -28,6 +28,11 @@ def test_shift_cities():
     road_map1 = [("Kentucky", "Frankfort", 38.197274, -84.86311),
                  ("Delaware", "Dover", 39.161921, -75.526755),
                  ("Minnesota", "Saint Paul", 44.95, -93.094)]
+
+    assert shift_cities(road_map1) == [("Delaware", "Dover", 39.161921, -75.526755),
+                                       ("Kentucky", "Frankfort", 38.197274, -84.86311),
+                                       ("Minnesota", "Saint Paul", 44.95, -93.094)]
+
 
 
     '''add your tests'''
