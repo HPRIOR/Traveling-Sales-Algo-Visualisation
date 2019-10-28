9b5d8cf573a357916cf5b58987543b6d8a6f0de5
HPRIOR <hprior02@dcs.bbk.ac.uk>
2019-10-28 21:50:28 +0000

    small comment on test_cities

--- test_cities.py
+++ test_cities.py
@@ -6,7 +6,8 @@ def test_compute_total_distance():
     road_map1 = [("Kentucky", "Frankfort", 38.197274, -84.86311),
                 ("Delaware", "Dover", 39.161921, -75.526755),
                 ("Minnesota", "Saint Paul", 44.95, -93.094)]
-    
+
+    # calculate this by hand then and give value at the end
     assert compute_total_distance(road_map1) == pytest.approx(9.386+18.496+10.646, 0.01)
 
 
