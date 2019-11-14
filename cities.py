f7bff8d26697d09620738299388ce9a8f19733d4
HPRIOR <hprior02@dcs.bbk.ac.uk>
2019-11-14 09:58:11 +0000

    added HO function to find min max of matrix

--- cities.py
+++ cities.py
@@ -218,7 +218,7 @@ def visualise(road_map):
     # main_win.geometry = '%sx%s' % (canvas_size_x + 100, canvas_size_y + 100)
     canv = Canvas(main_win, height=canvas_size_y, width=canvas_size_x)
     canv.pack()
-    canv.configure(highlightthickness=0, borderwidth=0)
+    #canv.configure(highlightthickness=0, borderwidth=0)
     ln = len(road_map)
     ind = 0
     for i in range(ln):
