2e9968a8bf8863d999e29848ddfe5135afeecf30
HPRIOR <hprior02@dcs.bbk.ac.uk>
2019-11-20 22:20:26 +0000

    Created edge around canvas

--- test.page.py
+++ test.page.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from cities import *
+import random
 
 '''root = Tk()
 root.title('This is an example window')
@@ -16,6 +17,30 @@ root.mainloop()
 
 '''
 
+def draw_line():
+    x1, y1, x2, y2 = random.randint(0, 390), random.randint(0, 390), random.randint(0, 390), random.randint(0,390)
+    c.create_line(x1, y1, x2, y2)
+
 
 root = Tk()
-root.geometry
+root.title('test')
+root.geometry("500x500")
+
+
+frame_1 = Frame(root, width=500, height=100,  bg='white')
+border = Frame(root, width=500, height=1, bg='black')
+frame_2 = Frame(root, width=500, height=399, bg='white')
+frame_2.pack_propagate(False)
+frame_1.pack_propagate(False)
+
+c = Canvas(frame_2, width=490, height=390, bg='linen')
+b = Button_1(Frame_1)
+
+
+frame_2.pack()
+border.pack()
+frame_1.pack()
+
+c.pack()
+
+root.mainloop()
