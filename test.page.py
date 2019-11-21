1a0b16a02232f7a2e587012e0256ccb5b327f7c5
HPRIOR <hprior02@dcs.bbk.ac.uk>
2019-11-21 23:45:18 +0000

    Created edge around canvas

--- test.page.py
+++ test.page.py
@@ -2,7 +2,7 @@ from tkinter import *
 from cities import *
 import random
 
-'''def draw_line():
+def draw_line():
     x1, y1, x2, y2 = random.randint(0, 490), random.randint(0, 390), random.randint(0, 490), random.randint(0, 390)
     c.create_line(x1, y1, x2, y2)
 
@@ -19,10 +19,10 @@ frame_2.pack_propagate(False)
 frame_1.pack_propagate(False)
 
 c = Canvas(frame_2, width=490, height=390, bg='linen')
-b = Button(frame_1, text='button', command=draw_line)
+b = Button(c, text='button', command=draw_line)
+c.pack_propagate(False)
+c.create_window(20,100, window=b)
 
-
-b.pack()
 frame_2.pack()
 border.pack()
 frame_1.pack()
@@ -30,7 +30,4 @@ frame_1.pack()
 c.pack()
 
 root.mainloop()
-'''
 
-list_1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(sorted(list_1[1]))
